a8a88348544316a3f21d6735ffbcd4e1b16da0c6
Martijn den Hoedt <martijnn2008@gmail.com>
2015-10-06 12:14:33 +0200

    added search form, need some javascript for that

--- movies/generate.py
+++ movies/generate.py
@@ -5,12 +5,15 @@ def main():
         movies = [e.strip() for e in file]
         nr_of_movies = len(movies)
         movies = '\n'.join(movies)
+    with open('search_form.txt', 'r') as file:
+        search_form = file.read()
     with open('end.txt', 'r') as file:
         end = file.read()
     with open('index.html', 'w') as file:
         file.write(start)
         file.write('<p class="lead">The ' + str(nr_of_movies) + ' ')
         file.write('movies I have seen are listed on this page.</p>\n')
+        file.write(search_form)
         file.write('<p class="movies">\n')
         file.write(movies)
         file.write(end)
