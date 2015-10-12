c4db6b00207481ee3e6a02ff94b2a2d27cbe70e8
Martijn den Hoedt <martijnn2008@gmail.com>
2015-10-12 17:08:38 +0200

    sort function on year works

--- movies/movies.js
+++ movies/movies.js
@@ -1,16 +1,39 @@
 var movie_array;
+var sort_year, sort_title;
 
 function set_up() {
     temp = document.getElementById("movies-list").innerHTML;
-    movie_array = temp.split("\n");
+    movie_array = temp.trim().split("\n");
+    sort_year = document.getElementById("year-sort");
+    sort_title = document.getElementById("title-sort");
+}
+
+function extract_year(str) {
+    year = str.match(/\(.*(19|20)\d{2}\)/)[0];
+    year = year.match(/\d{4}/)[0];
+    return parseInt(year);
 }
 
 function movies_sort_year() {
-
+    sort_year.className = "glyphicon glyphicon-ok";
+    sort_title.className = "blank glyphicon glyphicon-ok";
+    movie_array.sort(function(a, b) {
+        year_a = extract_year(a);
+        year_b = extract_year(b);
+        return year_a - year_b;
+    });
+    output = movie_array.join("\n");
+    document.getElementById("movies-list").innerHTML = output;
 }
 
 function movies_sort_title() {
-
+    sort_year.className = "blank glyphicon glyphicon-ok";
+    sort_title.className = "glyphicon glyphicon-ok";
+    movie_array.sort(function compareYear(a, b) {
+        return Math.random() * 100 % 10;
+    });
+    output = movie_array.join("\n");
+    document.getElementById("movies-list").innerHTML = output;
 }
 
 function movies_search() {
