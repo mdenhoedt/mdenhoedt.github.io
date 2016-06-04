7ee7c4e966f2c05b3a0fa23e236fd3bbc8323bb0
Martijn den Hoedt <martijnn2008@gmail.com>
2016-06-04 14:44:55 +0200

    added last Hunger Games

--- movies_statistics/statistics.js
+++ movies_statistics/statistics.js
@@ -3,6 +3,7 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
             ['Movie', 'Rating']
+            ,['The Hunger Games: Mockingjay - Part 2 (2015)', 6.6]
             ,['Captain America: Civil War (2016)', 8.3]
             ,['Everest (2015)', 7.1]
             ,['Spotlight (2015)', 8.1]
