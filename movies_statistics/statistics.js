aaa5c40c56846522357aa7c75073b2dc79ce98b7
Martijn den Hoedt <martijnn2008@gmail.com>
2016-08-09 23:36:28 +0200

    added eye in the sky

--- movies_statistics/statistics.js
+++ movies_statistics/statistics.js
@@ -3,6 +3,7 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
             ['Movie', 'Rating']
+            ,['Eye in the Sky (2015)', 7.4]
             ,['Shoot 'Em Up (2007)', 6.7]
             ,['2 Guns (2013)', 6.7]
             ,['Man on a Ledge (2012)', 6.6]
