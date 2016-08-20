12f04a0a37463be7aad9698e6c64bcf495c9e06f
Martijn den Hoedt <martijnn2008@gmail.com>
2016-08-20 14:36:13 +0200

    added Mr. Holmes

--- movies_statistics/statistics.js
+++ movies_statistics/statistics.js
@@ -3,6 +3,7 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
             ['Movie', 'Rating']
+            ,['Mr. Holmes (2015)', 6.9]
             ,['Click (2006)', 6.4]
             ,['Whiskey Tango Foxtrot (2016)', 6.6]
             ,['Under sandet (2015)', 7.8]
