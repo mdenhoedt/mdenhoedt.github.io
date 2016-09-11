b9651f6bba493dea97310ccbe76c30aceee9d207
Martijn den Hoedt <martijnn2008@gmail.com>
2016-09-11 16:48:02 +0200

    added Batman Begins

--- movies_statistics/statistics.js
+++ movies_statistics/statistics.js
@@ -3,6 +3,7 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
             ['Movie', 'Rating']
+            ,['Batman Begins (2005)', 8.3]
             ,['Oblivion (2013)', 7.0]
             ,['Prisoners (2013)', 8.1]
             ,['Reservoir Dogs (1992)', 8.4]
