f9eb85c1f48ae37d654fd8825e59bfbaf3090e10
Martijn den Hoedt <martijnn2008@gmail.com>
2016-10-12 22:59:35 +0200

    added RED

--- movies_statistics/statistics.js
+++ movies_statistics/statistics.js
@@ -3,6 +3,7 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
             ['Movie', 'Rating']
+            ,['RED', 7.1]
             ,['I Am Wrath', 5.3]
             ,['Badge of Honor', 4.8]
             ,['X2', 7.5]
