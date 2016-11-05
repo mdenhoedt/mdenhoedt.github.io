bdd8eda16b6b575c7cbbb8b298ae33471e87781b
Martijn den Hoedt <martijnn2008@gmail.com>
2016-11-05 18:44:52 +0100

    Jason Bourne (2016)

--- movies_statistics/statistics.js
+++ movies_statistics/statistics.js
@@ -3,6 +3,7 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
             ['Movie', 'Rating']
+            ,['Jason Bourne', 6.8]
             ,['The Forger', 5.7]
             ,['Hours', 6.5]
             ,['Predestination', 7.4]
