f1a3fa011b2ec6ea721cc7d3b538179c97c46380
Martijn den Hoedt <martijnn2008@gmail.com>
2016-12-04 15:40:52 +0100

    added Snowden

--- movies_statistics/statistics.js
+++ movies_statistics/statistics.js
@@ -3,6 +3,7 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
             ['Movie', 'Rating']
+            ,['Snowden', 7.4]
             ,['Doctor Strange', 7.9]
             ,['The Amazing Spider-Man 2', 6.7]
             ,['Spider-Man 3', 6.2]
